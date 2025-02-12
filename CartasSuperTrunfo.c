a4eb777c9b0862188bdf9790cc9454570fe82664
NicolasNCS <157858305+NicolasNCS@users.noreply.github.com>
2025-02-12 09:26:27 +0000

    Feito a lógica do programa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,13 +19,25 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     //Declaração das variáveis que serão utilizadas neste projeto.
-
     char estado;
-    char codigoCidade;
+    char codigoCidade[3];
     char nomeCidade[50];
-    int populacao, area, pontosTuristicos;
-    float pib;
+    int populacao, pontosTuristicos;
+    float pib, area;
 
+    printf("Digite a letra que representa o estado:\n");
+    scanf("%c", &estado);
+    printf("Digite agora o código da cidade(por exemplo, A01, A02, B01, B02)\n");
+    scanf("%s", &codigoCidade);
+    printf("Digite a população dessa cidade:\n");
+    scanf("%i", &populacao);
+    printf("Digite a área dessa cidade:\n");
+    scanf("%f", area);
+    printf("Digite os pontos turísticos dessa cidade:\n");
+    scanf("%i", &pontosTuristicos);
+    printf("Digite o PIB dessa cidade:\n");
+    scanf("%f", pib);
 
+    printf("Estado: %c\nCódigo da carta: %s\nNome da cidade: %s\nPopulação: %i\nÁrea: %f\nPIB: %f\nNúmero de pontos turísticos: %i\n", estado, codigoCidade, nomeCidade, populacao, area, pib, pontosTuristicos);
     return 0;
 }
